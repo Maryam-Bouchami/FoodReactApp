3ae8dabfa95d64f424cc7f5464d16266bfe7bdc4
ouss87 <zabrateiphone4@gmail.com>
2025-11-21 12:14:22 +0100

    add request to see a specific recipe using id

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function FoodDetails({ foodId }) {
-  return <div>{foodId}</div>;
+  const [food, setFood] = useState({});
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "24261700e5424760b7e96ad82e404c2e";
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+  return (
+    <div>
+      {foodId}
+      {food.title}
+      <img src={food.image} alt="" />
+    </div>
+  );
 }
